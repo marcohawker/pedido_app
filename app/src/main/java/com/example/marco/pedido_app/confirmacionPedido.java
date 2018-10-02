aaeed069e3a25d7997d2634309e99dafde57db53
marco <marco@asd.com>
2018-10-02 15:31:17 -0300

    2 de octubre
    2 de octubre

--- app/src/main/java/com/example/marco/pedido_app/confirmacionPedido.java
+++ app/src/main/java/com/example/marco/pedido_app/confirmacionPedido.java
@@ -6,13 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class confirmacionPedido extends AppCompatActivity {
-
+TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmacion_pedido);
+        tv1= (TextView)findViewById(R.id.textView6);
+
+    tipoComida1 tc1= new tipoComida1();
+        /*int tx3= Integer.parseInt(tc1.tx1);
+        int tx4= Integer.parseInt(tc1.tx2);
+        int tx3= 3;
+        int tx4= 2;*/
+            tv1.setText(Integer.toString(tc1.tx1 + tc1.tx2));
 
 
     }
--- app/src/main/java/com/example/marco/pedido_app/tipoComida1.java
+++ app/src/main/java/com/example/marco/pedido_app/tipoComida1.java
@@ -1,13 +1,36 @@
 package com.example.marco.pedido_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class tipoComida1 extends AppCompatActivity {
-
+    Button mbutton;
+    EditText mEditext1, mEditext2;
+    int tx1, tx2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipo_comida1);
+
+        mbutton = (Button)findViewById(R.id.button3);
+        mEditext1   = (EditText)findViewById(R.id.editText2);
+        mEditext2   = (EditText)findViewById(R.id.editText3);
+
+        mbutton.setOnClickListener(new View.OnClickListener()
+                {
+
+                    public void onClick(View view1)
+                    {
+                       tx1= Log.v("EditText", mEditext1.getText().toString());
+                        tx2= Log.v("EdiTex2",mEditext2.getText().toString());
+                        Intent intent= new Intent(tipoComida1.this,MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
     }
 }
